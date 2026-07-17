f06f5f00e45c151e590f2bb3cbc13486e84be98e
Guillermo Jiménez <guillermo.nez14@gmail.com>
2026-07-17 11:26:19 +0200

    Se aplica el experimento manky-fils

--- entrenar.py
+++ entrenar.py
@@ -2,6 +2,7 @@ import pandas as pd, joblib
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
+from sklearn.model_selection import cross_val_score
 import yaml, json
 
 p = yaml.safe_load(open("params.yaml"))["entrenar"]
@@ -16,7 +17,13 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=p["test_size
 modelo = RandomForestClassifier(n_estimators=p["n_estimators"], random_state=p["random_state"])
 modelo.fit(X_train, y_train)          # <-- AQUÍ ocurre el aprendizaje
 
-acc = accuracy_score(y_test, modelo.predict(X_test))
-json.dump({"accuracy": round(acc, 4)}, open("metrics.json", "w"))
+aacc = accuracy_score(y_test, modelo.predict(X_test))
+scores = cross_val_score(modelo, X_train, y_train, cv=5)
+
+json.dump({
+    "accuracy": round(aacc, 4),
+    "cv_mean": round(scores.mean(), 4),
+    "cv_std": round(scores.std(), 4),
+}, open("metrics.json", "w"))
 
 joblib.dump(modelo, "modelo.pkl")     # <-- el artefacto
